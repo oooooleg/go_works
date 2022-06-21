4278e2a2f6f86cad933f5077a96315ff50cb360b
Oleg Gubanov <o.gubanov@drweb.com>
2022-06-22 00:35:59 +0300

    first version

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,65 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	sb := &strings.Builder{}
+	var curRune rune
+	var escaped bool
+	var err error
+
+	for _, r := range s {
+		curRune, escaped, err = processRune(r, escaped, curRune, sb)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	if curRune != 0 {
+		sb.WriteRune(curRune)
+	}
+	return sb.String(), nil
+}
+
+func processRune(r rune, escaped bool, curRune rune, sb *strings.Builder) (rune, bool, error) {
+	if r == '\\' {
+		if escaped {
+			return r, false, nil
+		}
+		if curRune != 0 {
+			sb.WriteRune(curRune)
+		}
+		return 0, true, nil
+	}
+
+	if curRune == 0 {
+		if unicode.IsDigit(r) {
+			if !escaped {
+				return 0, false, ErrInvalidString
+			}
+		}
+		return r, false, nil
+	}
+
+	if !unicode.IsDigit(r) || escaped {
+		sb.WriteRune(curRune)
+		return r, false, nil
+	}
+
+	count := (int)(r - '0')
+
+	if count > 9 || count < 0 {
+		return 0, false, ErrInvalidString
+	}
+
+	for i := 0; i < count; i++ {
+		sb.WriteRune(curRune)
+	}
+
+	return 0, false, nil
 }
